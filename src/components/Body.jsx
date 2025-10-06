820c854a8367842f88ec8cb0d1787939273b49ec
APrajapat10 <coolprajapat@gmail.com>
2025-10-06 15:51:04 +0530

    Corrected imports

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,5 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import NavBar from "./NavBar";
+import NavBar from "./Navbar";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
